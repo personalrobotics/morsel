9759957c0294ec0c5ace7f34261a3430ebab2a5e
Pyry Matikainen <pkmatikainen@gmail.com>
2015-04-02 12:18:35 -0400

    stylistic changes

--- detector/biteserver.py
+++ detector/biteserver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Python libs
-import sys, time, math
+import math
 
 # numpy and scipy
 import numpy as np
@@ -10,30 +10,28 @@ import numpy as np
 import cv2
 
 # Ros libraries
-import roslib
 import rospy
 
 # Ros Messages
-from sensor_msgs.msg import CompressedImage
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseArray
 from geometry_msgs.msg import Pose
 
-# json and base64
-import base64
+# json
 import json
 
 # bite detector
 import bitefinder
 
-class AdaBiteServer:
+
+class AdaBiteServer(object):
 
     def __init__(self):
         self.VERBOSE = False
         self.finder = bitefinder.BiteFinder(debug=self.VERBOSE)
         self.ransac_iters = 200
-        self.ransac_thresh = 0.01 # 1cm thresh?
+        self.ransac_thresh = 0.01  # 1cm thresh?
         self.max_bites = 10
         self.bite_height = 0.01
         self.json_pub = None
@@ -44,17 +42,17 @@ class AdaBiteServer:
         self.fcount = 0
 
     def prepare_mask(self):
-    	self.raw_mask = cv2.imread(self.maskfn)
-    	layers = cv2.split(self.raw_mask)
-    	self.mask = np.zeros(layers[0].shape, dtype=np.float32)
-    	self.mask[layers[0] > 128] = 1.0
-    	print("Loaded mask")
+        self.raw_mask = cv2.imread(self.maskfn)
+        layers = cv2.split(self.raw_mask)
+        self.mask = np.zeros(layers[0].shape, dtype=np.float32)
+        self.mask[layers[0] > 128] = 1.0
+        print("Loaded mask")
 
     def decode_uncompressed_f32(self, data):
         if self.VERBOSE:
             print("Data has encoding: %s" % data.encoding)
         rows = data.height
-        cols = data.step / 4 # assume 32 bit depth
+        cols = data.step / 4  # assume 32 bit depth
         temp = np.fromstring(data.data, dtype=np.float32)
         temp = np.nan_to_num(temp)
         temp = temp.reshape(rows, cols)
@@ -68,21 +66,21 @@ class AdaBiteServer:
         return temp
 
     def set_intrinsics_from_fov(self, hfov, vfov, width, height):
-        # wlog say image plane at distance 1, then
+        # canonical image plane at distance 1, then
         # image_half_width/1 = tan(hfov)
         # image_half_height/1 = tan(vfov)
         # image_half_height = image_half_width / aspect
         cx = width / 2.0
         cy = height / 2.0
-        fx = (width / 2.0) / math.tan(hfov / 2.0) 
+        fx = (width / 2.0) / math.tan(hfov / 2.0)
         fy = (height / 2.0) / math.tan(vfov / 2.0)
-        self._projmat = np.array([[ fx, 0.0,  cx],
+        self._projmat = np.array([[fx, 0.0,  cx],
                                   [0.0,  fy,  cy],
                                   [0.0, 0.0, 1.0]])
         self._inv_projmat = np.linalg.inv(self._projmat)
         print(self._inv_projmat)
 
-    def set_intrinsics(self, K, zero_centered = True):
+    def set_intrinsics(self, K, zero_centered=True):
         self._K = K.copy()
         if zero_centered and self._normMat is not None:
             self._projmat = self._normMat.dot(K)
@@ -102,32 +100,31 @@ class AdaBiteServer:
         return [self._proj_pt(pt) for pt in pts]
 
     def _assign_bite_depth(self, pt, coeffs):
-        return (pt[0], pt[1], pt[0]*coeffs[0] + pt[1]*coeffs[1] + coeffs[2])
+        return (pt[0], pt[1], pt[0] * coeffs[0] + pt[1] * coeffs[1] + coeffs[2])
 
     def process_depth(self, img):
-    	self.fcount += 1
-    	if(self.fcount % self.decimate != 0):
-    		return
+        self.fcount += 1
+        if(self.fcount % self.decimate != 0):
+            return
 
         # first, fit a plane with ransac and get the residuals
         best_coeffs, num_inliers, residuals = bitefinder.ransac_plane(img,
-                                                self.ransac_iters, 
-                                                self.ransac_thresh)
-
+                                                                      self.ransac_iters,
+                                                                      self.ransac_thresh)
 
         if(num_inliers == 0 or residuals is None):
-        	if(self.VERBOSE):
-        		print("No plane found.")
-        	return
+            if(self.VERBOSE):
+                print("No plane found.")
+            return
 
         # find bite-sized things in the residuals
-        bites = self.finder.find_bites(residuals, 
+        bites = self.finder.find_bites(residuals,
                                        self.max_bites,
                                        -(self.bite_height))
 
         # estimate bite depths from plane fit
-        bite_positions = [self._assign_bite_depth(b[0], best_coeffs) 
-                            for b in bites]
+        bite_positions = [self._assign_bite_depth(b[0], best_coeffs)
+                          for b in bites]
 
         # compute projections
         pts_3d = self.project_points(bite_positions)
@@ -157,37 +154,37 @@ class AdaBiteServer:
     def callback_depth(self, data):
         img_base = self.decode_uncompressed_f32(data)
         self.process_depth(img_base)
-        
+
     def start_listening(self, depth_topic, json_pub_topic, ros_pub_topic):
         rospy.init_node('adabitefinder')
 
-        self.depth_sub = rospy.Subscriber(depth_topic, Image, 
-                                          self.callback_depth, queue_size = 1)
+        self.depth_sub = rospy.Subscriber(depth_topic, Image,
+                                          self.callback_depth, queue_size=1)
 
         self.json_pub = rospy.Publisher(json_pub_topic, String)
         self.ros_pub = rospy.Publisher(ros_pub_topic, PoseArray)
+
 
 def point_to_pose(p):
     ret = Pose()
     ret.position.x = p[0]
     ret.position.y = p[1]
     ret.position.z = p[2]
-    ret.orientation.w = 1.0 # make sure quat is normalized
+    ret.orientation.w = 1.0  # make sure quat is normalized
     return ret
+
 
 def deg_to_rad(d):
     return math.pi * (d / 180.0)
 
 if __name__ == '__main__':
 
-    import sys
-
     frame_listener = AdaBiteServer()
-    frame_listener.set_intrinsics_from_fov(deg_to_rad(70.95), deg_to_rad(55.00), 
-                                            320.0, 240.0)
-    frame_listener.start_listening( "/softkinetic_driver/depth/image",
-                                    "/perception/morsel_detection",
-                                    "/perception/morsel_pts")
+    frame_listener.set_intrinsics_from_fov(deg_to_rad(70.95), deg_to_rad(55.00),
+                                           320.0, 240.0)
+    frame_listener.start_listening("/softkinetic_driver/depth/image",
+                                   "/perception/morsel_detection",
+                                   "/perception/morsel_pts")
 
     # keep ros going
     rospy.spin()
